8020308e1b45c7baa56368b52d0b24e5af158181
Pete Latham <github@petelatham.com>
2019-03-14 12:55:13 +0000

    Version 1 - meets original requirements

---
+++ screens/DetailScreen.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Image, Text } from 'react-native';
+
+class DetailScreen extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('contact').name,
+    headerStyle: {
+      backgroundColor: '#2a3daa',
+    },
+    headerTintColor: '#ffffff',
+  });
+
+  capitalise = s => s[0].toUpperCase() + s.slice(1);
+
+  render() {
+    let {
+      name,
+      picture,
+      gender,
+      company,
+      address,
+      filmName,
+      filmUrl,
+    } = this.props.navigation.getParam('contact');
+    let isFemale = gender === 'female';
+    return (
+      <View style={styles.container}>
+        <Image style={styles.image} source={{ uri: picture }} />
+        <View style={styles.detailBox}>
+          <Text style={styles.detail}>
+            {name} is {this.capitalise(gender)}
+          </Text>
+          <Text style={styles.detail}>
+            {isFemale ? 'She' : 'He'} lives at {address}
+          </Text>
+          <Text style={styles.detail}>
+            {isFemale ? 'She' : 'He'} works at {company}
+          </Text>
+          <Text style={styles.detail}>
+            {isFemale ? 'Her' : 'His'} favourite film is {filmName}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  detailBox: {
+    alignItems: 'flex-start',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginVertical: 10,
+    marginBottom: 10,
+  },
+  detail: {
+    fontSize: 18,
+    marginVertical: 5,
+  },
+});
+
+export default DetailScreen;
---
+++ screens/ListScreen.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+import contacts from '../assets/contacts.json';
+
+class ListScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: '',
+    };
+    this.onPress = this.onPress.bind(this);
+    this.renderItem = this.renderItem.bind(this);
+  }
+
+  static navigationOptions = {
+    title: 'Contacts',
+    headerStyle: {
+      backgroundColor: '#2a3daa',
+    },
+    headerTintColor: '#ffffff',
+  };
+
+  onPress(item) {
+    this.props.navigation.navigate('Detail', { contact: item });
+  }
+
+  keyExtractor(item, index) {
+    return `${index}`;
+  }
+
+  renderSeparator() {
+    const style = { height: 1, backgroundColor: '#777' };
+    return <View style={style} />;
+  }
+
+  renderItem({ item }) {
+    return (
+      <View>
+        <Text
+          underlayColor={'#e4e4e4'}
+          onPress={() => {
+            this.onPress(item);
+          }} // Use the anonymous function to gain control of the argument passing rather than a reference
+          style={styles.listItem}>
+          {item.name}
+        </Text>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <FlatList
+        data={contacts}
+        renderItem={this.renderItem}
+        keyExtractor={this.keyExtractor}
+        ItemSeparatorComponent={this.renderSeparator}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  listItem: {
+    height: 50,
+    fontSize: 18,
+    paddingLeft: 10,
+    paddingRight: 10,
+    backgroundColor: '#fff',
+  },
+  labelText: {
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 18,
+  },
+});
+
+export default ListScreen;
